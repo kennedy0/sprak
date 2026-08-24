f1be05a0b59ecfab150acc70e65e21425a822683
akennedy <andrewjacobkennedy@gmail.com>
2026-08-23 19:29:07 -0700

    sprak 2.0 wip

--- src/sprak/cli.py
+++ src/sprak/cli.py
@@ -1,7 +1,9 @@
 import logging
 import sys
 import time
-from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+import tomllib
+from argparse import ArgumentParser
+from pathlib import Path
 
 from sprak import pack
 from sprak.log import logger
@@ -10,22 +12,27 @@ from sprak.log import logger
 def pack_sprites() -> None:
     parser = ArgumentParser(prog="sprak", description="Pack sprites into an atlas.")
     parser.add_argument("src", nargs="+", help="the source path(s) to scan for sprites")
-    parser.add_argument("-v", "--verbose", action="count", default=0)
+    parser.add_argument("--fps", default=24, type=float, help="set GIF frame rate")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="set verbosity; can be used multiple times to increase verbosity",
+    )
+    parser.add_argument("--version", action="version", version=_get_version_str())
 
     group = parser.add_argument_group("output")
-    group.add_argument("--zip", metavar="FILE", help="write the atlas to a ZIP file")
-    group.add_argument("--json", metavar="FILE", help="write the atlas to a JSON file")
-    group.add_argument("--png", metavar="FILE", help="write the atlas to a PNG file")
-    group.add_argument("--gif", metavar="FILE", help="write the packing animation to a GIF file")
-    group.add_argument(
-        "--debug-gif", metavar="FILE", help="write the packing animation (with debug info) to a GIF file"
-    )
-    group.add_argument("--fps", default=24, type=float, help="")
+    group.add_argument("--zip", metavar="FILE", help="write atlas to a ZIP file")
+    group.add_argument("--json", metavar="FILE", help="write atlas to a JSON file")
+    group.add_argument("--png", metavar="FILE", help="write atlas to a PNG file")
+    group.add_argument("--gif", metavar="FILE", help="write packing animation to a GIF file")
+    group.add_argument("--debug-gif", metavar="FILE", help="write packing animation with debug info to a GIF file")
 
     args = parser.parse_args(sys.argv[1:])
 
     if not args.zip and not args.json and not args.png and not args.gif and not args.debug_gif:
-        parser.error("at lest one of --zip, --json, --png, --gif, or --debug-gif is required")
+        parser.error("at lest one output option is required")
 
     if args.verbose >= 2:
         logger.setLevel(logging.DEBUG)
@@ -45,6 +52,13 @@ def pack_sprites() -> None:
         fps=args.fps,
     )
     logger.info(f"sprites packed in {round(time.time() - start, 3)}s")
+
+
+def _get_version_str() -> str:
+    pyproject_toml = Path(__file__).parent.parent.parent / "pyproject.toml"
+    with pyproject_toml.open("rb") as fp:
+        data = tomllib.load(fp)
+        return data.get("project", {}).get("version", "")
 
 
 if __name__ == "__main__":
